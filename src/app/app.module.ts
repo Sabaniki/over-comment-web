24b75d1f8cad389f851c38c60d44d6d90e6d5cd9
Sabaniki <sawada13@mccc.jp>
2020-07-06 18:45:13 +0900

    コメント送信可能。クリックだけじゃなくControl+Enterで送信にも対応。今から投票機能を作る。

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,23 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CommentComponent } from './comment-page/comment/comment.component';
 import {RouterModule} from '@angular/router';
+import {AngularFireModule} from '@angular/fire';
+import {firebaseConfig} from '../environments/firebase.secret';
+import {FormsModule} from '@angular/forms';
+import { VoteComponent } from './comment-page/vote/vote.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CommentComponent
+    CommentComponent,
+    VoteComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/comment-page/comment/comment.component.ts
+++ src/app/comment-page/comment/comment.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {AngularFirestore, AngularFirestoreCollection} from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-comment',
@@ -7,11 +8,21 @@ import {Component, OnInit} from '@angular/core';
 })
 export class CommentComponent implements OnInit {
 
-  constructor() {
+  private commentsCollection: AngularFirestoreCollection;
+  private today: Date;
+  public commentText: string;
+
+  constructor(private afs: AngularFirestore) {
   }
+
 
   ngOnInit(): void {
-    console.log('comments');
+    this.today = new Date();
+    this.commentsCollection = this.afs.collection<Comment>(this.today.toDateString());
+    console.log(this.today.toDateString());
   }
 
+  onClickSendCommentButton() {
+    this.commentsCollection.add({text: this.commentText}).then(() => this.commentText = '');
+  }
 }
